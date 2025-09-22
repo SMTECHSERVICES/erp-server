7f273265c7a53ef4cf476f65a5b3771fb0448cef
SMTECHSERVICES <abhishekmishara59@gmail.com>
2025-09-22 16:48:36 +0530

    add mongo url

--- utils/connectDatabase.js
+++ utils/connectDatabase.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 
 
 
 export async function conncectDb() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/erp');
+  await mongoose.connect(process.env.MONGO_URI);
   console.log('Database connected')
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
